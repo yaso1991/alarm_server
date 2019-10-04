a9ea06e1f8fec645665649c52887ed09868b2203
Yaso <1721662545@qq.com>
2019-10-04 11:22:02 +0800

    开发版 V1.0 1. 起点.                √

---
+++ src/main/java/club/yaso91/alarm_server/AlarmServerApplication.java
@@ -0,0 +1,13 @@
+package club.yaso91.alarm_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlarmServerApplication.class, args);
+  }
+
+}
